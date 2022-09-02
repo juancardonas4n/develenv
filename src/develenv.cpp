c9ad0cce263fb9eb090d3ac8b94f0010efc6805a
juancardonas4n <juancardona@seven4n.com>
2022-09-02 14:11:11 -0500

    Eliminate tabulators

--- src/develenv.cpp
+++ src/develenv.cpp
@@ -19,52 +19,46 @@ int
 main(void) {
 
   pid_t child;
-  
+
   if (existTmuxSession("devel:")) {
 
     child = fork();
     if (child == 0) {
       std::string currdesktop{::getenv("XDG_CURRENT_DESKTOP")};
       if (currdesktop == "GNOME") {
-	execl(GNOME_TERMINAL_PATH,
-	      GNOME_TERMINAL,
-	      "--command",
-	      "tmux attach -t devel",
-	      GNOME_FULL_SCREEN_OPTION,
-	      NULL);
-	exit(1);
+        execl(GNOME_TERMINAL_PATH,
+              GNOME_TERMINAL,
+              "--command",
+              "tmux attach -t devel",
+              GNOME_FULL_SCREEN_OPTION,
+              NULL);
+        exit(1);
       }
       else if (currdesktop == "XFCE") {
-	execl(XFCE_TERMINAL_PATH,
-	      XFCE_TERMINAL,
-	      "-e",
-	      "tmux attach -t devel",
-	      XFCE_FULL_SCREEN_OPTION,
-	      NULL);
+        execl(XFCE_TERMINAL_PATH,
+              XFCE_TERMINAL,
+              "-e",
+              "tmux attach -t devel",
+              XFCE_FULL_SCREEN_OPTION,
+              NULL);
       }
     }
   }
   else {
-    
+
     child = fork();
 
     if (child == 0) {
       std::string currdesktop(::getenv("XDG_CURRENT_DESKTOP"));
       if (currdesktop == "GNOME") {
-	execl(GNOME_TERMINAL_PATH,
-	      GNOME_TERMINAL,
-	      GNOME_FULL_SCREEN_OPTION,
-	      "--window-with-profile",
-	      "Desarrollo Programas",
-	      NULL);
       }
       else if (currdesktop == "XFCE") {
-	execl(XFCE_TERMINAL_PATH,
-	      XFCE_TERMINAL,
-	      XFCE_FULL_SCREEN_OPTION,
-	      "--command=tmux",
-	      // "--font=\"Monospace Regular 12\"",
-	      NULL);
+        execl(XFCE_TERMINAL_PATH,
+              XFCE_TERMINAL,
+              XFCE_FULL_SCREEN_OPTION,
+              "--command=tmux",
+              // "--font=\"Monospace Regular 12\"",
+              NULL);
       }
       exit(1);
     }
@@ -80,12 +74,8 @@ bool existTmuxSession(const char* sname) {
 
     if (fgets(p, BUFFER_SIZE, pIn) == NULL) break;
 
-    // std::cout << p << std::endl;
-    
     sscanf(p, "%s", pFirst);
 
-    // std::cout << "pFirst: " << pFirst << std::endl;
-    
     if (::strcmp(pFirst, sname) == 0) return true;
   } while (true);
 
